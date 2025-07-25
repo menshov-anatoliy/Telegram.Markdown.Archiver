5b6999a4f6442c3ec6f155705e7eb2596d72aca8
Анатолий Меньшов <menshov.anatoliy@gmail.com>
2025-07-25 12:18:01 +0300

    Add project files.

---
+++ Telegram.Markdown.Archiver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Telegram.Markdown.Archiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
